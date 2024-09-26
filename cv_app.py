a4d3601c2aaede00db0d70bd10170aedfe7d320c
24809055@sun.ac.za <24809055@sun.ac.za>
2024-09-26 23:12:58 +0200

    removed unused endpoint

--- cv_app.py
+++ cv_app.py
@@ -6,9 +6,6 @@ app = Flask(__name__, template_folder='./templates')
 def index():
     return render_template('./display _cv.html')
 
-@app.route('/return_cv')
-def return_pdf():
-    pass
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
